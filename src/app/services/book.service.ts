69a0c244e1468b545ec20ebc9a64c463c7a9d96c
tuananh0737 <rox.tuananh@gmail.com>
2025-12-30 16:56:11 +0700

    update

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -8,7 +8,7 @@ import { environment } from '../../environments/environment';
   providedIn: 'root'
 })
 export class BookService {
-  private apiUrl = '${environment.apiUrl}/public/find-all-book';
+  private apiUrl = `${environment.apiUrl}/public/find-all-book`;
 
   constructor(private http: HttpClient) { }
 
